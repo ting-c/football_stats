af4b84b9bf9f90b0cb6682b671adbf2b1a652cff
ting-c <ting.cheong31@gmail.com>
2020-06-11 21:35:20 +0100

    updated public folder after build

---
+++ public/build/precache-manifest.574f4eb4225ee296228e537668ba2131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2191a9f49d6047534829b697d9e4195",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ff9e4900b91e7baf573",
+    "url": "/static/css/main.1267f751.chunk.css"
+  },
+  {
+    "revision": "38886d34a0669a82c86e",
+    "url": "/static/js/2.83dce875.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.83dce875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ff9e4900b91e7baf573",
+    "url": "/static/js/main.acd61ffd.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
